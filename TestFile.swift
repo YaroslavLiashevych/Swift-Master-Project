9a8029092d44e7e4ecc0ceb5209a3dc5b30af76b
Yaroslav Liashevych <126164471+visibilityishidden@users.noreply.github.com>
2026-05-10 23:33:58 +0200

    fix: smart inline suggestions fallback and triggered code diff

--- TestFile.swift
+++ TestFile.swift
@@ -2,13 +2,13 @@ import Foundation
 
 class DetailViewController: UIViewController {
     var completionHandler: (() -> Void)?
-    var name: String?
+    var name2: String?
 
     func setup() {
         completionHandler = {
-            print(self.name!)
+            print(self.name2!)
         }
-        let upperName = name!.uppercased()
+        let upperName = name2!.uppercased()
         print(upperName)
     }
 }
